5587fcd952558c116c2a0471de1cda3db3ad0085
Aldrin <aldrinmcaalim@gmail.com>
2021-05-12 21:26:35 -0400

    CFunctional project
    will work on making it prettier

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const APP = express();
+const PORT = 7000;
+const PATH = require("path");
+const postData = require("./data.json");
+
+APP.use(express.static(PATH.join(__dirname, "public")));
+
+APP.set("view engine", "ejs");
+APP.set("views", PATH.join(__dirname, "/views"));
+
+// HOME PAGE
+APP.get("/", (req, res) => {
+  res.send(`GET request on "/" on port ${PORT}`);
+});
+
+// POSTS PAGE
+APP.get("/p/:subpost", (req, res) => {
+  const { subpost } = req.params;
+  const data = postData[subpost];
+  res.render("subpost", { ...data });
+});
+
+// PORT LISTENER
+APP.listen(PORT, () => {
+  console.log(`LISTENING ON PORT: ${PORT}`);
+});
